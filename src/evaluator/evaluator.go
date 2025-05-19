8a16e0023e253ae327cc96387024db6ad8f7258b
aeremic <andrija.eremic@gmail.com>
2025-05-19 19:38:54 +0200

    added infix opp evaluation

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -28,6 +28,13 @@ func Eval(node ast.Node) value.Wrapper {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	case *ast.IntegerLiteral:
 		return &value.Integer{
 			Value: node.Value,
@@ -41,4 +48,74 @@ func Eval(node ast.Node) value.Wrapper {
 	}
 
 	return nil
+}
+
+func evalBangOperatorExpression(right value.Wrapper) value.Wrapper {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right value.Wrapper) value.Wrapper {
+	if right.Type() != value.INTEGER {
+		return NULL
+	}
+
+	v := right.(*value.Integer).Value
+	return &value.Integer{
+		Value: -v,
+	}
+}
+
+func evalPrefixExpression(operator string, right value.Wrapper) value.Wrapper {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left value.Wrapper, right value.Wrapper) value.Wrapper {
+	lv := left.(*value.Integer).Value
+	rv := right.(*value.Integer).Value
+
+	switch operator {
+	case "+":
+		return &value.Integer{
+			Value: lv + rv,
+		}
+	case "-":
+		return &value.Integer{
+			Value: lv - rv,
+		}
+	case "*":
+		return &value.Integer{
+			Value: lv * rv,
+		}
+	case "/":
+		return &value.Integer{
+			Value: lv / rv,
+		}
+	default:
+		return NULL
+	}
+}
+
+func evalInfixExpression(operator string, left value.Wrapper, right value.Wrapper) value.Wrapper {
+	switch {
+	case left.Type() == value.INTEGER && right.Type() == value.INTEGER:
+		return evalIntegerInfixExpression(operator, left, right)
+	default:
+		return NULL
+	}
 }
